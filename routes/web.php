0e1b993ac110c13f1815074c629a18d4139d03fd
mourenemillie <lifeasnanaw@gmail.com>
2026-04-15 19:53:47 +0700

    fix(routes): add root redirect to user-management

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserManagementController;
 
+Route::get('/', function () {
+    return redirect('/user-management');
+});
+
 Route::get('/user-management', [UserManagementController::class, 'index'])->name('user-management.index');
 Route::get('/user-management/create', [UserManagementController::class, 'create'])->name('user-management.create');
 Route::post('/user-management', [UserManagementController::class, 'store'])->name('user-management.store');
